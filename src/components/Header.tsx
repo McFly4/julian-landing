a5b7ceda0bdd4799ffec1f6536d7a2708f76ab71
McFly <matthieu.roman2002@gmail.com>
2024-11-24 16:46:32 +0100

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default async function Header() {
+  return (
+    <nav className="fixed w-full bg-[#070A16] border-effect px-4 xl:px-16 py-6 flex items-center justify-between overflow-hidden">
+      <Image src="/logo.svg" alt="Company Logo" width={120} height={45} />
+      <div className="hidden xl:flex gap-10 ">
+        <Link href="#whoami">Qui je suis</Link>
+        <Link href="#program">Notre accompagnement</Link>
+        <Link href="#reviews">Avis clients</Link>
+      </div>
+      <Link href="#pricing">
+        <button className="hidden xl:flex custom-btn">
+          Vos visuels en un clic avec l&apos;IA
+        </button>
+      </Link>
+      <div className="xl:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Image src="/icons/burger.svg" alt="Menu" width={24} height={24} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>
+              <Link href="#whoami">Qui je suis</Link>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="#program">Notre accompagnement</Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="#reviews">Avis clients</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+}
