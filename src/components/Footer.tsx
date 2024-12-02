8302249e33dd4748c4ee07e91f9312244b86f162
McFly <matthieu.roman2002@gmail.com>
2024-12-02 22:58:21 +0100

    fix links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,18 @@ export default function Footer() {
   return (
     <footer className="my-20 px-4 md:px-20">
       <div className="flex justify-between">
-        <img src="/logo.svg" alt="logo" />
-        <img src="/linkedin.svg" alt="linkeidn" />
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/julian-caille-b01950207/"
+        >
+          <img src="/logo.svg" alt="logo" />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/julian-caille-b01950207/"
+        >
+          <img src="/linkedin.svg" alt="linkeidn" />
+        </a>
       </div>
       <p className="text-[#ffffff80] clashFont mt-3">
         © 2024 Agence LaLanding!
